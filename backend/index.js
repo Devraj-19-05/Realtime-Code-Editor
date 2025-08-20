d3dd52fb1f4fb19c4179be5f27f63c100ae4eab0
Devraj-19-05 <devrajjadhav869@gmail.com>
2025-08-21 00:03:08 +0530

    updated files: index.js,App.jsx,App.css

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,36 @@ const io = new Server(server, {
         origin:"*",
     },
 });
+const rooms = new Map();
 
-io.on("connection",(socket) => {
+io.on("connection", (socket) => {
     console.log("User Connected", socket.id);
+
+    let currentRoom = null;
+    let currentUser = null;
+
+    socket.on("join",({roomId, userName}) => {
+        if(currentRoom){
+            socket.leave(currentRoom);
+            rooms.get(currentRoom).delete(currentUser);
+            io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+        }
+
+        currentRoom = roomId;
+        currentUser = userName;
+
+        socket.join(roomId);
+
+        if (!rooms.has(roomId)){
+            rooms.set(roomId, new Set());
+        }
+
+        rooms.get(roomId).add(userName);
+
+        io.to(roomId).emit("userJoined", Array.from
+                (rooms.get(currentRoom)));
+        
+    });
 });
 
 const port = process.env.PORT || 5000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,38 @@ import { useState } from "react";
 import "./App.css";
 import io from 'socket.io-client';
 
-const socket = io("http://localhost:5000");
+const socket = io("http://localhost:5173");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const joinRoom = () =>{
+    console.log(roomId, userName);
+  }
 
   if(!joined) {
-  return <div>App not Joined</div>;
+    return (
+    <div className="join-container">
+      <div className="join-form">
+        <h1>Join Code Room</h1>
+        <input 
+        type="text" 
+        placeholder="Room Id"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+        />
+        <input 
+        type="text" 
+        placeholder="Your Nme"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+  </div>
+    );
   };
   return <div>User Joined</div>;
 };
