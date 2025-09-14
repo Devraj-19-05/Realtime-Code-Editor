b34a07fa3263a0b28da4b7bb0a2a6634d1cb5cb3
Jalp007 <23bca007@gsfcuniversity.ac.in>
2025-09-14 19:24:44 +0530

    Completed till Language Chanage Functionality in YouTube parts.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import io from 'socket.io-client';
 import Editor from '@monaco-editor/react'
+import { useEffect } from "react";
 
 const socket = io("http://localhost:5000");
 
@@ -12,6 +13,46 @@ const App = () => {
   const [language, setLanguage] = useState("javascript");
   const [code, setCode] = useState("");
   const [copySuccess, setCopySuccess] = useState("");
+  const [users, setUsers] = useState([]);
+  const [typing, setTyping] = useState("");
+
+  useEffect(()=>{
+    socket.on("userJoined",(users)=>{
+      setUsers(users);
+    });
+
+    socket.on("codeUpdate",(newCode)=>{
+      setCode(newCode);
+    });
+
+    socket.on("userTyping",(user)=>{
+      setTyping(`${user.slice(0,8)}... is typing`);
+      setTimeout(() => setTyping(""), 2000);
+    });
+
+    socket.on("languageUpdate",(newLanguage)=>{
+      setLanguage(newLanguage);
+    });
+
+    return () => {
+      socket.off("userJoined");
+      socket.off("codeUpdate");
+      socket.off("userTyping");
+      socket.off("languageUpdate");
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      socket.emit("leaveRoom");
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
 
   const joinRoom = () =>{
     if (roomId && userName) {
@@ -28,7 +69,15 @@ const App = () => {
 
   const handleCodeChange = (newCode) => { 
     setCode(newCode);
+    socket.emit("codeChange", {roomId, code:newCode}); 
+    socket.emit("typing", {roomId, userName});
   };
+
+  const handleLanguageChange = e => {
+    const newLanguage = e.target.value;
+    setLanguage(newLanguage);
+    socket.emit("languageChange", {roomId, language:newLanguage});
+  }
 
   if(!joined) {
     return (
@@ -66,13 +115,15 @@ const App = () => {
         </div>
         <h3>User in Room:</h3>
         <ul>
-          <li>Prashant</li>
-          <li>Xyz</li>
+          {users.map((user,index)=>(
+              <li key={index}>{user.slice(0,8)}</li>
+          ))}
+          
         </ul>
-        <p className="typeing-indicator">User typeing...</p>
+        <p className="typeing-indicator">{typing}</p>
         <select className="language-selector" 
         value={language} 
-        onChange={(e) => setLanguage(e.target.value)}
+        onChange={handleLanguageChange}
         >
           <option value="javascript">Javascript</option>
           <option value="python">Python</option>
